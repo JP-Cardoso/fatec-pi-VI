aeb214b33c48f9e760a3d8ba10f0cf70485d3c68
jp-cardoso <jhonpoal@hotmail.com>
2024-05-25 00:13:21 -0300

    add: conexão com o bando de dados e reorganização do projeto

--- src/infra/config/mysql/database-config.js
+++ src/infra/config/mysql/database-config.js
@@ -1,6 +1,9 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
+  'your_database_name',
+  'your_username',
+  'your_password',
   {
     host: 'localhost',
     dialect: 'mysql'
